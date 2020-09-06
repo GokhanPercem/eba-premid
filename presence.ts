b8ea2e9359786d8645605c084cf8d61858336281
EnesKaplan34 <54679572+EnesKaplan34@users.noreply.github.com>
2020-09-06 18:04:45 +0300

    presence

---
+++ presence.ts
@@ -0,0 +1,37 @@
+let presence = new Presence({
+    clientId: "000000000000000000" // Discord'dan (https://discordapp.com/developers/applications) aldığınız uygulama ID'si
+}),
+
+yazilar = await presence.getStrings({
+    oynatiliyor: "presence.playback.playing",
+    durduruldu: "presence.playback.paused"
+    // Çevirileri almak için bu fonksiyonu kullanabilirsiniz
+});
+
+
+
+
+presence.on("UpdateData", async () => {
+    /* UpdateData kullanıcı serviste gezdiği süre boyunca sürekli gönderilecektir, bu yüzden büyük işlemler kullanıcıları etkilememesi için buranın dışında tutulmalıdır. Burayı dikkatli kullanın.
+
+    Daha ağır ve sürekli tekrarlanmaması gereken işlemleri bu alanın dışında yukarıdaki gibi başka fonksiyon kullanarak yaptırmak önerilir. */
+
+    var presenceData = {
+        largeImageKey: "EBA - ANA SAYFA", /* Servisin kullanıcının profilinde gözükeceği büyük resmin adı. Buraya yazacağınız resimler, oluşturduğunuz uygulamanın "Rich Presence > Art Assets" kısmına yüklenmeli ve yüklendiği ismiyle girilmiş olmalıdır.
+        smallImageKey: "anahtar", /* Servisin kullanıcının profilinde gözükeceği küçük resminin adı. Buraya yazacağınız resimler, oluşturduğunuz uygulamanın "Rich Presence > Art Assets" kısmına yüklenmeli ve yüklendiği ismiyle girilmiş olmalıdır. */
+        smallImageText: "Falan da filan", // Küçük resmin üzerine gelindiğinde gözükecek yazı.
+        details: "Eğitim Videosu İzliyor.", // Üst kısımda gözükecek yazı.
+        state: "Ana Sayfa",  // Alt kısımda gözükecek yazı.
+        //startTimestamp: 1577232000, // Unix Epoch biçiminde yazılmış zaman verisi
+        //endTimestamp: 1577151472000 // Eğer "kaldı" biçiminde bir veri göstermek istiyorsanız, bitiş zamanını da aynı biçimde burada belirtmelisiniz.
+    }; /* Eğer isterseniz burada sadece belli bir şey belirtebilir veya hiç belirtmeden daha sonra bunları belirtebilirsiniz. Bunun için de presenceData.state = "Ana Sayfa"; yapabilirsiniz. */
+
+    if (presenceData.details == null) {
+        // Bu kısım presenceData objesinde "details" anahtarı bulunmadığı zaman devreye girecektir.
+        presence.setTrayTitle(); // Mac kullanıcıları için menü yazısını temizler.
+        presence.setActivity(); // Bu şekilde fonksiyona bir veri girmeden girerseniz, büyük resim Discord uygulamasının simgesine dönüşecek ve başka bir bilgi gösterilmeyecektir.
+    } else {
+        // Yukarıdaki durumun dışında herhangi bir şey gerçekleşirse burası devreye girecektir.
+        presence.setActivity(presenceData); // Aktiviteyi belirtilen verilerle ayarlar.
+    }
+});
